1e145cf21857ad5a8fe68a9a4e2810afcd83ffcc
David Santiago Garcia Chicangana <dsgarcia@unicauca.edu.co>
2023-03-29 11:40:39 -0500

    clscls

--- week-1/contracts/require.sol
+++ week-1/contracts/require.sol
@@ -2,7 +2,9 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract PokemonFactory{
+import "hardhat/console.sol";
+
+contract PokemonFactoryRequire{
 
   struct Pokemon{
     uint id;
@@ -18,7 +20,18 @@ contract PokemonFactory{
   mapping (uint => address) public pokemonToOwner;  // 01 -> xasasas
   mapping (address => uint) ownerPokemonCount;      // xasasas -> 01
 
+  //Pure bc we are not going to modify a state variable
+  function getStringLength(string memory str) public pure returns(uint){
+    bytes memory s = bytes(str);
+    return s.length;
+  }
+
   function createPokemon(uint _id, string memory _name) public{
+    require(_id > 0, "Id must to be greater than zero");
+    uint name_size = getStringLength(_name);
+    console.log(name_size);
+    require(name_size > 0, "Name must not be empty");
+    require(name_size > 2, "Name length must be greather than two");
     Pokemon memory p = Pokemon(_id, _name);
     pokemons.push(p);
     pokemonToOwner[_id] = msg.sender;
--- week-1/test/test_events.js
+++ week-1/test/test_events.js
@@ -1,30 +1,71 @@
 const { expect } = require("chai");
 const { BigNumber } = require("@ethersproject/bignumber");
 
-describe("Pokemon contract", function () {
+/**
+ * 'describe' function create a new test suite with the name given in the 1 parameter
+ */
+describe("PokemonContract", function () {
 
-  it("Pokemon Factory shouldn't has pokemons", async function () {
+  let myContract;
 
-    const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
-    const hardhatPokemon = await PokemonFactory.deploy();
-    
+  //beforeEach is a Mocha test framework hook that is called before each test case in a test suite.
+  //It is used to set up any necessary state or dependencies for the tests to run.
+  beforeEach(async function(){
+    //Create a new instance of the contract
+    const MyContract = await ethers.getContractFactory("PokemonFactoryRequire");
+    //Deploy the contract
+    myContract = await MyContract.deploy();
+  });
+
+  it("Event emitted when pokemon is created", async function () {
     const id = 15;
     const name = "Pikachu";
 
     //Even the id was converted to a BigNumber, the hardhat found it not appropiate for the test.
     //So, we have to conver the object to an array of primitive types.
-
     //From this
     //const expectedArgs = { id: BigNumber.from(id), name };
-
     //To this
     const expectedArgs = [BigNumber.from(id), name];
 
     //The createPokemon Functions emits the event 'eventNewPokemon'
     //with a 'pokemon' parameter. So, in this way, we can check if the event
     //was emitted with the correct data.
-    await expect(hardhatPokemon.createPokemon(id, name))
-      .to.emit(hardhatPokemon, "eventNewPokemon")
+    await expect(myContract.createPokemon(id, name))
+      .to.emit(myContract, "eventNewPokemon")
       .withArgs(expectedArgs);
   });
+
+  it("Should revert when id is less than or equal to zero", async function () {    
+    const id = 0;
+    const name = "Pikachu";
+    await expect(myContract.createPokemon(id, name)).to.be.revertedWith(
+      "Id must to be greater than zero"
+    );
+  });
+
+  it("Should no revert when id is greater than zero", async function () {    
+    const id = 1;
+    const name = "Pikachu";
+    myContract.createPokemon(id, name);
+    const pokemons = await myContract.getAllPokemons();
+    expect(pokemons.length).to.equal(1);
+  });
+
+  it("Should revert when name is empty", async function(){
+    const id = 1;
+    const name = "";
+    await expect(myContract.createPokemon(id, name)).to.be.revertedWith(
+      "Name must not be empty"
+    );
+  });
+
+  it("Should revert when name length is less than or equal to 2", async function(){
+    const id = 1;
+    const name = "pi";
+    await expect(myContract.createPokemon(id, name)).to.be.revertedWith(
+      "Name length must be greather than two"
+    );
+  });
+
 });
