df1232ab19f9e7c758b92b6baa50fc432df519b7
David Santiago Garcia Chicangana <dsgarcia@unicauca.edu.co>
2023-03-29 15:25:48 -0500

    Challenge #5 Creation of two pokemons done. Now I'm super sayayin Blue!😎

--- week-1/test/test_types.js
+++ week-1/test/test_types.js
@@ -39,5 +39,26 @@ describe("PokemonFactoryTypes", function () {
     expect(weaknesses_obtained.length).to.equal(1);
   });
 
+  it("Should create two pokemons", async function(){
+    const id = 1;
+    const name = "pikachu";
+    const pokemonType = ["Electrico"];
+    const weaknesses = ["Tierra"];
+    const skill_name = "Impactrueno";
+    const skill_description = "Rayo electrico que produce daño en el enemigo";
+
+    const id2 = 2;
+    const name2 = "Bulbasaur";
+    const pokemonType2 = ["Planta","Veneno"];
+    const weaknesses2 = ["Fuego","Volador","Hielo"];
+    const skill_name2 = "Espesura";
+    const skill_description2 = "Potencia sus movimientos de tipo Planta cuando le quedan pocos PS.";
+
+    myContract.createPokemon(id, name, pokemonType, weaknesses, skill_name, skill_description)
+    myContract.createPokemon(id2, name2, pokemonType2, weaknesses2, skill_name2, skill_description2)
+
+    const pokemons = await myContract.getAllPokemons();
+    expect(pokemons.length).to.equal(2);
+  });
 
 });
